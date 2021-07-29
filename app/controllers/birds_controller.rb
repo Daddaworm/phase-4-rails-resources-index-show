6900f309dadce7a4cefc1f28c2acf041f0a72c44
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-29 12:00:50 -0400

    Completed assignment

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,26 @@
+class BirdsController < ApplicationController
+
+    # GET /birds
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+
+    #GET /birds/:id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else 
+            render json: { error: "Bird not found" }, status: :not_found
+        end
+    end
+
+    # DELETE /birds/:id
+    def destroy
+        bird = Bird.find_by(:id params[:id]).destroy
+        render json: bird
+    end
+
+
+end
